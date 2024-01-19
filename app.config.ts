f34d0ba16fa72fd4e1ab9495876a6067212dbd39
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2024-01-20 01:51:46 +0200

    feat: Remove test styles

--- app.config.ts
+++ app.config.ts
@@ -1,33 +1,5 @@
 export default defineAppConfig({
   ui: {
-    
 
-    button: {
-      base: 'leading-6',
-      rounded: 'rounded-[10px]',
-      size: {
-        md: 'text-md',
-      },
-      padding: {
-        md: 'px-6 py-2'
-      },
-      gap: {
-        md: 'gap-x-1'
-      },
-      color: {
-        'tamer-grey': {
-          solid: 'bg-tamer-grey-100 text-white hover:bg-tamer-grey-90 disabled:bg-tamer-grey-80 active:bg-tamer-grey-100'
-        }
-      },
-      default: {
-        size: 'md',
-        color: 'tamer-grey'
-      },
-      icon: {
-        size: {
-          md: 'w-6 h-6'
-        }
-      }
-    }
   }
 })
