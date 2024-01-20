18e683b8223c7026e668b8e2dd109971bbffb0ce
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2024-01-20 02:40:19 +0200

    feat: Add login page

---
+++ layouts/hero.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="overflow-y-none flex h-screen flex-col items-center justify-center">
+    <slot />
+  </div>
+</template>
--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -1,11 +1,29 @@
 <script setup lang="ts">
 definePageMeta({
-  layout: 'auth'
+  layout: 'hero'
 })
 </script>
 
 <template>
-  <div>
-    <h1 class="text-4xl font-bold">Login</h1>
+  <div class="absolute right-6 top-6 flex flex-row gap-2">
+    <ColorPicker />
+    <ColorModeButton />
+  </div>
+
+  <div class="flex flex-col gap-4 rounded-xl bg-white p-6 dark:bg-gray-800 dark:border-gray-700 border">
+    <h1 class="text-3xl font-bold">
+      Log in
+    </h1>
+
+    <p>Log in to get access to great gaming experience</p>
+
+    <UButton
+      size="xl"
+      variant="outline"
+      block
+      icon="i-logos-google-icon"
+    >
+      Log in with Google
+    </UButton>
   </div>
 </template>
