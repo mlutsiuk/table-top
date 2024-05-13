2a0aa7593b9c1cf1007ff73e418c7e94eafdd603
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2024-05-13 14:44:05 +0300

    feat: Install trpc

---
+++ pages/trpc.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const { $client } = useNuxtApp()
+
+const hello = await $client.hello.query({ text: 'client' })
+</script>
+
+<template>
+  <div v-if="hello">
+    <p>{{ hello }}</p>
+  </div>
+</template>
