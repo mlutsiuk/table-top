1728eb6f73bac2e7a305fa74ec655c66003d67e3
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2024-09-01 02:42:03 +0300

    feat: Add basic layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,22 @@
 <template>
-  <div class="overflow-y-none h-screen">
-    <slot />
+  <div class="flex min-h-screen flex-col items-stretch">
+    <div class="flex flex-row items-center justify-between border-b p-6 shadow-sm">
+      <div class="flex flex-row items-center gap-4">
+        <UIcon name="i-fa6-solid-dice-d20" class="size-6" />
+
+        <div class="text-3xl font-medium">
+          Table-Top
+        </div>
+      </div>
+
+      <div class="flex flex-row items-center gap-2">
+        <ColorPicker />
+        <ColorModeButton />
+      </div>
+    </div>
+
+    <div class="grow p-6">
+      <slot />
+    </div>
   </div>
 </template>
