62b455ccb06defa1db7df18c1522b1d4784196e2
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2024-09-01 15:23:34 +0300

    feat: Move AppHeader to separate component

---
+++ components/AppHeader.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+const { user } = storeToRefs(useAuthStore())
+</script>
+
+<template>
+  <div class="flex flex-row items-center justify-between gap-6 border-b p-6 shadow-sm">
+    <div class="flex flex-row items-center gap-4">
+      <UIcon name="i-fa6-solid-dice-d20" class="size-6" />
+
+      <div class="text-3xl font-medium">
+        Table-Top
+      </div>
+    </div>
+
+    <div class="grow">
+      <UButton
+        variant="link"
+        color="white"
+        :to="{ name: 'campaigns' }"
+        label="Campaigns"
+      />
+    </div>
+
+    <div class="flex flex-row items-center gap-2">
+      <ColorPicker />
+      <ColorModeButton />
+
+      <UAvatar
+        :alt="user!.name"
+        size="md"
+      />
+    </div>
+  </div>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,19 +1,6 @@
 <template>
   <div class="flex min-h-screen flex-col items-stretch">
-    <div class="flex flex-row items-center justify-between border-b p-6 shadow-sm">
-      <div class="flex flex-row items-center gap-4">
-        <UIcon name="i-fa6-solid-dice-d20" class="size-6" />
-
-        <div class="text-3xl font-medium">
-          Table-Top
-        </div>
-      </div>
-
-      <div class="flex flex-row items-center gap-2">
-        <ColorPicker />
-        <ColorModeButton />
-      </div>
-    </div>
+    <AppHeader />
 
     <div class="grow p-6">
       <slot />
