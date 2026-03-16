b2e72d941a90bdea7040fe3360f57d3c75dd1385
mlutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2026-03-17 01:55:11 +0200

    feat: Migrate from Nuxt ui to shadcn-vue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,10 +6,31 @@ export default defineNuxtConfig({
 
   modules: [
     '@nuxt/devtools',
+    '@nuxtjs/tailwindcss',
     '@pinia/nuxt',
     '@vueuse/nuxt',
-    '@nuxt/ui'
+    'shadcn-nuxt',
+    '@nuxt/icon'
   ],
+
+  shadcn: {
+    /**
+     * Prefix for all the imported component.
+     * @default "Ui"
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * Will respect the Nuxt aliases.
+     * @link https://nuxt.com/docs/api/nuxt-config#alias
+     * @default "@/components/ui"
+     */
+    componentDir: '@/components/ui'
+  },
+  icon: {
+    mode: 'css',
+    cssLayer: 'base'
+  },
 
   imports: {
     dirs: [
@@ -17,7 +38,7 @@ export default defineNuxtConfig({
     ]
   },
 
-  css: ['~/assets/css/main.css'],
+  css: ['~/assets/css/tailwind.css'],
 
   devtools: { enabled: true },
 
