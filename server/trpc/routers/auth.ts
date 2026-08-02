1d46d5e8fd9cdc6ed557708333d27c832ff58b4d
mlutsiuk <mlutsiuk.pm@gmail.com>
2026-08-02 22:52:22 +0300

    fix: Skip optional google oauth consent screen

--- server/trpc/routers/auth.ts
+++ server/trpc/routers/auth.ts
@@ -37,8 +37,6 @@ export const authRouter = router({
 
   getAuthUrl: publicProcedure.query(() => {
     const redirectUrl = getGoogleOauthClient().generateAuthUrl({
-      access_type: 'offline',
-      prompt: 'consent',
       scope: ['email', 'profile']
     })
 
