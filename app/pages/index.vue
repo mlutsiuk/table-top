3ec1334edd7e24da7eb895d19ccad41fe3be2b6f
mlutsiuk <mlutsiuk.pm@gmail.com>
2026-08-02 22:56:35 +0300

    fix: Remove fetch for users on index page, redirect to campaigns instead

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,30 +1,6 @@
 <script setup lang="ts">
 definePageMeta({
-  middleware: 'auth'
+  middleware: 'auth',
+  redirect: { name: 'campaigns' }
 })
-
-const { data, status } = useFetch('/api/users')
 </script>
-
-<template>
-  <div class="flex flex-col gap-3 rounded border-2 border-border px-4 py-8">
-    <pre>{{ data }}</pre>
-
-    {{ status }}
-
-    <h2 class="font-mono text-4xl">
-      Its index page
-    </h2>
-
-    <hr>
-    <div>
-      <Button
-        variant="destructive"
-        :trailing="false"
-      >
-        Button
-        <Icon name="tabler:chevron-right" /> 
-      </Button>
-    </div>
-  </div>
-</template>
